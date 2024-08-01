0b781bced020eb8bad2f2171ea6e43bee6039888
Astaethiel <marciniak.miko@gmail.com>
2024-08-01 14:23:06 +0100

    create helper script to locate all page paths

---
+++ src/app/_helpers/getAllRoutes.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
+const appDir = path.join(__dirname, "app");
+
+function scanDirectory(dir, routePrefix = "") {
+  const files = fs.readdirSync(dir);
+
+  files.forEach((file) => {
+    const fullPath = path.join(dir, file);
+    const stat = fs.statSync(fullPath);
+
+    if (stat.isDirectory()) {
+      if (file.startsWith("[")) {
+        // Dynamic route
+        console.log(`${routePrefix}/${file.replace(/[\[\]]/g, ":")}`);
+      } else {
+        scanDirectory(fullPath, `${routePrefix}/${file}`);
+      }
+    } else if (file === "page.js") {
+      console.log(routePrefix || "/");
+    }
+  });
+}
+
+scanDirectory(appDir);
