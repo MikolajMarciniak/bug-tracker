a1350a26b3240bff922938a68f8358347eebfe93
Astaethiel <marciniak.miko@gmail.com>
2024-08-02 16:13:31 +0100

    fix deployment script

--- src/app/_components/SideBar.js
+++ src/app/_components/SideBar.js
@@ -5,10 +5,13 @@ export default function Sidebar() {
     <aside className="sidebar" role="banner">
       <ul>
         <li>
-          <a href="/dashboard">Dashboard</a>
+          <a href="/projects">Dashboard</a>
         </li>
         <li>
           <a href="/login">Login</a>
+        </li>
+        <li>
+          <a href="/register">Register</a>
         </li>
       </ul>
     </aside>
